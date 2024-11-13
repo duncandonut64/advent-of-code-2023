f35de0da5d8c7d323407a239abd274fc04f73911
dpatchen@umich.edu <dpatchen@umich.edu>
2024-11-13 09:56:52 -0500

    Added checking functions for detecting non-digit or non-period char

--- day3/main.py
+++ day3/main.py
@@ -3,7 +3,82 @@ input_file = open("day3-input.txt").read()
 input_list = input_file.splitlines()
 line_length = len(input_list[0])
 
-print(line_length)
+def checkCharacter(r, c):
+    return not (input_list[r][c].isdigit() or input_list[r][c] == ".")
+
+def checkBR(r, c):
+    result = checkCharacter(r + 1, c)
+    result = result or checkCharacter(r, c + 1)
+    result = result or checkCharacter(r + 1, c + 1)
+    return result
+
+def checkBL(r, c):
+    result = checkCharacter(r + 1, c)
+    result = result or checkCharacter(r, c - 1)
+    result = result or checkCharacter(r + 1, c - 1)
+    return result
+
+def checkTR(r, c):
+    result = checkCharacter(r - 1, c)
+    result = result or checkCharacter(r, c + 1)
+    result = result or checkCharacter(r - 1, c + 1)
+    return result
+
+def checkTL(r, c):
+    result = checkCharacter(r - 1, c)
+    result = result or checkCharacter(r, c - 1)
+    result = result or checkCharacter(r - 1, c - 1)
+    return result
+
+def checkR(r, c):
+    result = checkCharacter(r + 1, c)
+    result = result or checkCharacter(r - 1, c)
+    result = result or checkCharacter(r - 1, c + 1)
+    result = result or checkCharacter(r, c + 1)
+    result = result or checkCharacter(r + 1, c + 1)
+    return result
+
+def checkL(r, c):
+    result = checkCharacter(r + 1, c)
+    result = result or checkCharacter(r - 1, c)
+    result = result or checkCharacter(r - 1, c - 1)
+    result = result or checkCharacter(r, c - 1)
+    result = result or checkCharacter(r + 1, c - 1)
+    return result
+
+def checkT(r, c):
+    result = checkCharacter(r, c - 1)
+    result = result or checkCharacter(r - 1, c - 1)
+    result = result or checkCharacter(r - 1, c)
+    result = result or checkCharacter(r - 1, c + 1)
+    result = result or checkCharacter(r, c + 1)
+    return result
+
+def checkB(r, c):
+    result = checkCharacter(r, c - 1)
+    result = result or checkCharacter(r + 1, c - 1)
+    result = result or checkCharacter(r + 1, c)
+    result = result or checkCharacter(r + 1, c + 1)
+    result = result or checkCharacter(r, c + 1)
+    return result
+
+def checkAll(r, c):
+    result = checkCharacter(r, c - 1)
+    result = result or checkCharacter(r + 1, c - 1)
+    result = result or checkCharacter(r + 1, c)
+    result = result or checkCharacter(r + 1, c + 1)
+    result = result or checkCharacter(r, c + 1)
+    result = result or checkCharacter(r - 1, c + 1)
+    result = result or checkCharacter(r - 1, c)
+    result = result or checkCharacter(r - 1, c - 1)
+    return result
+
+
+print(input_list)
+
+
+
+
 
 # Outline:
 # Make a function to detect whether or not on an edge (one for each so LR TD)
